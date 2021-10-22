1c3349e3717ba58b279e42ced7e80ada9a17738d
Anier-98 <anierf3@gmail.com>
2021-10-22 14:29:28 -0600

    clase doc en blanco

---
+++ Singleton/src/main/java/org/anier/Doc_en_Blanco.java
@@ -0,0 +1,21 @@
+package org.anier;
+
+import java.util.ArrayList;
+
+public class Doc_en_Blanco {
+
+    private static Doc_en_Blanco _instance = null;
+
+    private <Documento> Doc_en_Blanco()
+    {
+        ArrayList<Documento> Documentos = new ArrayList<Documento>();
+    }
+
+    public static Doc_en_Blanco Instance()
+    {
+        if (_instance == null)
+            _instance = new Doc_en_Blanco();
+        return _instance;
+    }
+
+}
