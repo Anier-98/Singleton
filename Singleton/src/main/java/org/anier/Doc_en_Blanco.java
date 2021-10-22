5abb01108238ee37123844e92f9c7544c889db90
Anier-98 <anierf3@gmail.com>
2021-10-22 14:40:02 -0600

    clase comercial

--- Singleton/src/main/java/org/anier/Doc_en_Blanco.java
+++ Singleton/src/main/java/org/anier/Doc_en_Blanco.java
@@ -2,13 +2,14 @@ package org.anier;
 
 import java.util.ArrayList;
 
-public class Doc_en_Blanco {
+public class Doc_en_Blanco
+{
 
     private static Doc_en_Blanco _instance = null;
 
     private <Documento> Doc_en_Blanco()
     {
-        ArrayList<Documento> Documentos = new ArrayList<Documento>();
+        ArrayList<Documento> Documento = new ArrayList<Documento>();
     }
 
     public static Doc_en_Blanco Instance()
